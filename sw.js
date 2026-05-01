f1500fa8414d1985633781f403c79c7f6d991e74
Willyan Souza Brito <willyanszbrito@gmail.com>
2026-05-01 14:27:14 -0400

    feat: enable PWA (app functionality) with manifest and service worker

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'teacher-lupi-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './site.webmanifest',
+  './mascot.jpg',
+  './android-chrome-192x192.png',
+  './android-chrome-512x512.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        return cachedResponse || fetch(event.request);
+      })
+  );
+});
